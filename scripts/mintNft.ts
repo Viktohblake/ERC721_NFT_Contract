e33c4f2c494f4045b2c86381680a1c4c96d9ef67
Viktohblake <damanygt@gmail.com>
2024-02-23 16:43:35 +0100

    Fix NFT not displaying

--- scripts/mintNft.ts
+++ scripts/mintNft.ts
@@ -8,20 +8,18 @@ async function main() {
 
   // Get the deployed contract instance
   const contractFactory = await ethers.getContractFactory("VNFT");
-  const vnft: any = contractFactory.attach("0x1c31D708178358231141d258979b709e720028dC");
+  const vnft: any = contractFactory.attach("0xDf77F373a9BD58CeE7Db9De7043f2CB5F9cAD3e5");
   console.log("Contract address:", vnft.getAddress);
 
   // Instantiate the IPFS Url for the NFT
-  const ipfsUrl = "https://ipfs.io/ipfs/QmaP5tJD9rqUM6MfMkcfy4NAFSm1UgCh4YYyvcGcy7rWvg";
+  const ipfsUrl = "https://ipfs.io/ipfs/QmRNQPpLQ5nco37Qz1rZ2ARGL4z1o4NmSefk3dhG5EhST4";
 
   const recipient = (await ethers.provider.getSigner()).getAddress();
   console.log("Recipient address", recipient);
   const mintTransaction = await vnft.mintNFT(recipient, ipfsUrl);
   // get mint transaction info e.g. status, gas used etc
   const transactionInfo = await mintTransaction.wait();
-  const tokenId = transactionInfo.events[0].args.tokenId;
-  console.log(`Minted NFT with token Id ${tokenId}`)
-
+  console.log("Transaction Info", transactionInfo);
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
